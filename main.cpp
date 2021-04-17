ded18f63a0fe2eac58be75924d13175fa005d8b9
BAV <ke1717bav34@susu.ru>
2021-04-17 17:54:59 +0500

    Added input suming

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ using namespace std;
 int main()
 {
 int a,b;
-cout<<"Enter two integer numbers"<<end1;
+cout<<"Enter two integer numbers"<<endl;
 cin>>a>>b;
+int c;
+c= a+b;
+cout << "a+b = " << c << endl;
+c = a-b;
+cout << "a-b = " << c << endl;
 }
